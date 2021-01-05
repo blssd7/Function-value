21dce1f4ca63cdd6f56865165cc7713f0b720a2c
blssd7 <74246910+blssd7@users.noreply.github.com>
2021-01-05 19:45:52 +0300

    find the value of the expression

---
+++ src/com/gmail/valevach/task1/Task1_01.java
@@ -0,0 +1,13 @@
+package com.gmail.valevach.task1;
+
+public class Task1_01 {
+
+        public static void main (String[] args){
+            int a = 7;
+            int b = 8;
+            int c = 10;
+            int z = ((a - 3) * b/2) + c;
+            System.out.println(z);
+        }
+    }
+
