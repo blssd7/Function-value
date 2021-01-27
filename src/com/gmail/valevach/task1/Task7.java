b44e87b74c1e9fa66694e070c9c911a032fb9388
Renata Valevach <valevachrenata@gmail.com>
2021-01-27 11:18:54 +0300

    Expression value by formula

---
+++ src/com/gmail/valevach/task1/Task7.java
@@ -0,0 +1,36 @@
+package com.gmail.valevach.task1;
+//  Нахождение углов треугольника определение прямоугольного треугольника
+    import java. util.Scanner ;
+    import java. lang. Math;
+
+public class Task7{
+    public static void main (String[] args) {
+      int a = 1;
+      int b = 1;
+      int c;
+     if ((a = b) > c {
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
